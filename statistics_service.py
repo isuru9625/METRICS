85eb474d33f10f7e4c2e013fb08891a24d69102c
isuru9625 <isurufernando.16@cse.mrt.ac.lk>
2023-12-08 14:36:22 +0530

    changed json params

--- statistics_service.py
+++ statistics_service.py
@@ -177,7 +177,7 @@ def get_statistics(github_username, repo_owner, repo_name, access_token = None):
             'num_commits_last_two_weeks': num_commits_last_two_weeks,
             'num_user_commits': num_user_commits,
             'num_lines_changed' : num_lines_changed, 
-            'git_version': 'V1'
+            'git_version': 'V2'
         }
     else:
         return None
