6c95fb05d0d224450cba0fb3b900a8c92f32190f
isuru9625 <isurufernando.16@cse.mrt.ac.lk>
2024-01-08 14:46:00 +0530

    changed the version to 13

--- statistics_service.py
+++ statistics_service.py
@@ -185,8 +185,6 @@ def insert_metrics_into_mysql(metrics):
         cursor = connection.cursor()
         
 
-        # Assuming you have a table named 'metrics' with columns
-        # 'github_usernamee', 'num_commits_last_two_weeks', 'num_user_commits', 'num_lines_changed'
         insert_query = '''
             INSERT INTO metrics (github_username, num_commits_last_two_weeks, num_user_commits, num_lines_changed)
             VALUES (%s, %s, %s, %s)
@@ -234,7 +232,7 @@ def get_statistics(github_username, repo_owner, repo_name, access_token = None):
             'num_commits_last_two_weeks': num_commits_last_two_weeks,
             'num_user_commits': num_user_commits,
             'num_lines_changed' : num_lines_changed, 
-            'git_version': 'V12'
+            'git_version': 'V13'
         }
     else:
         return None
